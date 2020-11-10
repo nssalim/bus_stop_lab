fdb2f6722027eb437975d4b19590dcb4c8017eb9
Nargis <nargis.salim@btinternet.com>
2020-11-10 16:59:03 +0000

    first commit

---
+++ src/bus.py
@@ -0,0 +1,15 @@
+class Bus:
+    def __init__(self, route, destination):
+        self.route_number = route
+        self.destination = destination
+ 
+    
+    def drive(self):
+        return "Brum brum"
+
+    
+
+  
+
+    
+
---
+++ src/bus_stop.py
@@ -0,0 +1,6 @@
+class BusStop:
+    def __init__(self, name):
+        self.name = name
+        self.queue =[]
+
+    
