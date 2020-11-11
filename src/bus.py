e770cc1bd782668872eab0b2ae321d136804ae15
Nargis <nargis.salim@btinternet.com>
2020-11-11 08:13:18 +0000

    an update

--- src/bus.py
+++ src/bus.py
@@ -1,13 +1,36 @@
 class Bus:
-    def __init__(self, route, destination):
+    def __init__(self, route, destination,):
         self.route_number = route
         self.destination = destination
- 
+        self.passengers = []
+     
+    def route(self):
+        route = 22
+        return route
+
+    def destination():
+        destination = "Ocean Terminal"
+        return destination 
     
     def drive(self):
         return "Brum brum"
 
+    def starts_with_no_passengers(self):
+        passengers = [0]
+        return passengers
+
+    def can_pick_up_passenger(self, person):
+        passenger = passengers
+        passenger_count += [person]
+        return passengers.append(person)
+
+
+    def can_drop_off_passenger(self, person):
+        passenger = passengers
+        passenger_count  -= [person]
+        return passengers.remove(person)
     
+
 
   
 
--- src/person.py
+++ src/person.py
@@ -2,7 +2,25 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.passenger_1 = passenger_1
+        self.passenger_2 = passenger_2
+     
 
+    def name():
+        return name["Guido van Rossum"]
     
+    def age():
+        return [64] 
 
+    # person_1  = passenger_1()
     
+    # passenger_1 = [name]
+    # bus.pick_up(self, passenger_1)
+
+    # passenger_2 = [name]
+    # bus.pick_up(self, passenger_2)
+
+    # bus.drop_off(self, passenger_2)
+
+#pop to remove
+#append to add
